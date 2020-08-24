c6acca1a2fe057cf6d46cdfe017216cd9f541220
Dang Nguyen Hai <dangnh@nhg.vn>
2020-08-24 16:26:10 +0700

    feature send mail

--- config/config.exs
+++ config/config.exs
@@ -29,3 +29,19 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
+
+# Myapp.Email.welcome_email() |> Myapp.Mailer.deliver_now
+config :myapp, Myapp.Mailer,
+  adapter: Bamboo.SMTPAdapter,
+  server: "smtp.office365.com",
+  #hostname: "www.mydomain.com",
+  port: 587,
+  username: "dangnh@nhg.vn", # or {:system, "SMTP_USERNAME"}
+  password: "Santazero1", # or {:system, "SMTP_PASSWORD"}
+  tls: :if_available, # can be `:always` or `:never`
+  allowed_tls_versions: [:"tlsv1", :"tlsv1.1", :"tlsv1.2"],
+  # or {":system", ALLOWED_TLS_VERSIONS"} w/ comma seprated values (e.g. "tlsv1.1,tlsv1.2")
+  ssl: false, # can be `true`,
+  retries: 1,
+  no_mx_lookups: false, # can be `true`
+  auth: :if_available # can be `always`.
--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Myapp.MixProject do
   def application do
     [
       mod: {Myapp.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :bamboo]
     ]
   end
 
@@ -44,7 +44,9 @@ defmodule Myapp.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:bamboo, "~> 1.5"},
+      {:bamboo_smtp, "~> 2.1"}
     ]
   end
 
