51482cc0370e42bca13caf3b7576d8299b61165d
Dang Nguyen Hai <dangnh@nhg.vn>
2020-08-25 11:48:58 +0700

    set mail

--- lib/myapp/email.ex
+++ lib/myapp/email.ex
@@ -2,12 +2,11 @@ defmodule Myapp.Email do
   import Bamboo.Email
 
   def welcome_email do
-    new_email(
-      to: "b0yb0m113@gmail.com",
-      from: "dangnh@nhg.vn",
-      subject: "Welcome to the app.",
-      html_body: "<strong>Thanks for joining!</strong>",
-      text_body: "Thanks for joining!"
-    )
+    new_email
+    |> from("dangnh@nhg.vn")
+    |> to("b0yb0m113@gmail.com")
+    |> subject("Welcome!!!")
+    |> html_body("<strong>Welcome</strong>")
+    |> text_body("Thanks for joining!")
   end
 end
--- lib/myapp/send_mail.ex
+++ lib/myapp/send_mail.ex
@@ -3,7 +3,7 @@ defmodule Myapp.SendMail do
   alias Myapp.Mailer
 
   def send_mail do
-    Email.welcome_email()   # Create your email
-    |> Mailer.deliver_now() # Send your email
+    Email.welcome_email   # Create your email
+    |> Mailer.deliver_now # Send your email
   end
 end
