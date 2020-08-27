dedeaac627c507a0f6eb229684d74fc85a62a830
Dang Nguyen Hai <dangnh@nhg.vn>
2020-08-27 15:32:11 +0700

    add code

--- lib/myapp_web/controllers/calendar_controller.ex
+++ lib/myapp_web/controllers/calendar_controller.ex
@@ -1,9 +1,46 @@
 defmodule MyappWeb.CalendarController do
   use MyappWeb, :controller
+  use Timex
+
+
+
+  @week_start_at :mon
+
+
+  defp day_names(:sun), do:  [7, 1, 2, 3, 4, 5, 6] |> Enum.map(&Timex.day_shortname/1)
+  defp day_names(_), do:  [1, 2, 3, 4, 5, 6, 7] |> Enum.map(&Timex.day_shortname/1)
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    current_date = Timex.now
+
+    assigns = [
+      current_date: current_date,
+      day_names: day_names(@week_start_at),
+      week_rows: week_rows(current_date)
+    ]
+
+    render(conn, "index.html", assigns)
   end
 
+  # def render(assigns) do
+  #   TutorialWeb.PageView.render("calendar.html", assigns)
+  # end
+
+
+  defp week_rows(current_date) do
+    first =
+      current_date
+      |> Timex.beginning_of_month()
+      |> Timex.beginning_of_week(@week_start_at)
+
+    last =
+      current_date
+      |> Timex.end_of_month()
+      |> Timex.end_of_week(@week_start_at)
+
+    Interval.new(from: first, until: last)
+    |> Enum.map(& &1)
+    |> Enum.chunk_every(7)
+  end
 
 end
